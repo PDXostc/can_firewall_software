da6c693926d21948ceacbeaca223b2e668102ae8
Steve Miller <smiller6@jaguarlandrover.com>
2015-09-10 17:01:37 -0700

    create rule from working set
    properly shifts in the values of dtoperand parcels into the single large
    dtoperand number in the final rule.

--- canbus-firewall-avr/src/rules.c
+++ canbus-firewall-avr/src/rules.c
@@ -33,6 +33,7 @@ void print_rule(rule_t *rule) {
     print_dbg("\n\r ID Operand:  \n\r");
     print_dbg_hex(rule->idoperand);
     print_dbg("\n\r Data Operand:  \n\r");
+    print_dbg_hex(rule->dtoperand >> 32);
     print_dbg_hex(rule->dtoperand);
     
     print_dbg("____END_RULE____");
@@ -70,4 +71,26 @@ bool save_rule(rule_t *source_rule, rule_t *dest_rule)
     #endif
     success = true;
     return success;
+}
+
+rule_t create_rule_from_working_set(rule_working_t *working) {
+    rule_t new_rule;
+    
+    new_rule.prio =         working->prio;
+    new_rule.mask =         working->prep_01.mask;
+    new_rule.filter =       working->prep_02.filter;
+    new_rule.xform =        working->prep_01.xform;
+    new_rule.idoperand =    working->prep_04.idoperand;
+
+    //There's probably a much more efficient way to accomplish loading the uint16 parcels into the uint64    
+
+    new_rule.dtoperand = working->prep_03.dtoperand02[0];
+    new_rule.dtoperand = new_rule.dtoperand << 16;
+    new_rule.dtoperand |= working->prep_03.dtoperand02[1];
+    new_rule.dtoperand = new_rule.dtoperand << 16;
+    new_rule.dtoperand |= working->prep_03.dtoperand02[2];
+    new_rule.dtoperand = new_rule.dtoperand << 16;
+    new_rule.dtoperand |= working->prep_02.dtoperand01;
+    
+    return new_rule;
 }
