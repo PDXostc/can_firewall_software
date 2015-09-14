7bbda5deb6efb6ac805edb6f4c2c57fb26edd5c5
Steve Miller <smiller6@jaguarlandrover.com>
2015-09-14 13:00:25 -0700

    add sleep routine

---
+++ canbus-firewall-avr/src/sleep.h
@@ -0,0 +1,22 @@
+/*
+ * sleep.h
+ *
+ * Created: 9/14/2015 12:06:28 PM
+ *  Author: smiller6
+ */ 
+
+
+/*
+*  Sleep routine(s) for our main program
+*/
+
+#ifndef SLEEP_H_
+#define SLEEP_H_
+
+#include <asf.h>
+
+extern void sleep_mode_start(void);
+
+
+
+#endif /* SLEEP_H_ */
