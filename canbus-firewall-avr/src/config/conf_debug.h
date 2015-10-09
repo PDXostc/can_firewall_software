8185d60ce401b76b3712f0e12bea0bc7a3895f54
Steve Miller <smiller6@jaguarlandrover.com>
2015-10-09 10:47:52 -0700

    add comments and descriptions to functions previously without

--- canbus-firewall-avr/src/config/conf_debug.h
+++ canbus-firewall-avr/src/config/conf_debug.h
@@ -12,16 +12,14 @@
 #ifndef CONF_DEBUG_H_
 #define CONF_DEBUG_H_
 
-#define DBG_ON          1
-#define DBG_TEST        1
-#define DBG_CLKS        DBG_ON && 1
-#define DBG_CAN_MSG     DBG_ON && 1
-#define DBG_FLASH       DBG_ON && 1
-#define DBG_HMAC        DBG_ON && 1
-#define DBG_RULES       DBG_ON && 1
+#define DBG_ON          1			//master enable debug
+#define DBG_TEST        1			//temp, misc
+#define DBG_CLKS        DBG_ON && 1 //clocks
+#define DBG_CAN_MSG     DBG_ON && 1 //can messages
+#define DBG_FLASH       DBG_ON && 1 //flash
+#define DBG_HMAC        DBG_ON && 1 //hmac
+#define DBG_RULES       DBG_ON && 1 //rules
 
-//#define PRINT_DBG(x)        { if(DBG_ON) {print_dbg(const char *x);} }
-//#define PRINT_DBG_TEST      { if(DBG_TEST) {print_dbg("Test\n");} }
 #define PRINT_NEWLINE       { if(DBG_ON) {print_dbg("\n\r");} }
 
 #endif /* CONF_DEBUG_H_ */
--- canbus-firewall-avr/src/filter.h
+++ canbus-firewall-avr/src/filter.h
@@ -21,14 +21,57 @@
 // TODO: detect actual pin state dedicated to shunt
 static bool detected_shunt = DETECTED_SHUNT;
 
+/* Evaluation cases to be returned by evaluate function.
+ * DISCARD: No matching rule was found, please discard message
+ * NEW: Message matches hard coded id for new rules, please treat data as new rule frame
+ * FILTER: Message matches rule found in set, please filter accordingly
+ */
 const enum Eval_t {
 	DISCARD, NEW, FILTER
 } Eval_t;
 
+/**
+ * \brief Operate on id field of provided CAN message using operand value and 
+ * transform. Transforms are defined in rules and according to spec. Xform value
+ * must be provided (masked and shifted) so that it corresponds with the correct
+ * half byte value. For transforming an id, supply (xform = (xform_byte & 0xF0) >> 4)
+ * 
+ * \param msg Pointer to CAN message to be transformed
+ * \param rule_operand Value to modify id by
+ * \param xform Half byte value corresponding to predefined transform operation
+ * 
+ * \return extern int 0 == success, 1 == encountered block xform, -1 == encountered unexpected value
+ */
 extern int operate_transform_id(volatile can_msg_t *msg, U32 *rule_operand, int xform);
 
+/**
+ * \brief Operate on data field of provided CAN message using operand value and
+ * transform. Transforms are defined in rules and according to spec. Xform value
+ * must be provided (masked) so that it corresponds with the correct
+ * half byte value. For transforming an id, supply (xform = (xform_byte & 0x0F))
+ * 
+ * \param data Pointer to data payload to be transformed
+ * \param rule_operand Value to modify data by
+ * \param xform Half byte value corresponding to predefined transform operation
+ * 
+ * \return extern int 0 == success, 1 == encountered block xform, -1 == encountered unexpected value
+ */
 extern int operate_transform_u64(U64 *data, U64 *rule_operand, int xform);
 
+/**
+ * \brief Evalutate CAN message against provided ruleset. Returns enumeration for
+ * recommendation of how to handle message based on whether a match was found.
+ * If a match exists, pointer value to rule will be written to the out_rule.
+ * Note that if no match is found, the function will recommend DISCARD. For a NEW
+ * rule match, the boolean to check the physical shunt must be in place, and an id
+ * for new rules must be defined.
+ * 
+ * \param msg Pointer to message to evaluate
+ * \param ruleset Pointer to ruleset array to iterate message against
+ * \param out_rule Double pointer to hold address of matched rule, if found
+ * 
+ * \return extern enum Eval_t DISCARD == no match found, NEW == shunt connected and id matches new rule signature, FILTER == match found
+ */
 extern enum Eval_t evaluate(volatile can_mob_t *msg, rule_t *ruleset, rule_t **out_rule);
 
 #endif /* FILTER_H_ */
