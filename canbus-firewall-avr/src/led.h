c5a98439beee640999b61cdb4ed47811bb2bc22b
Steve Miller <smiller6@jaguarlandrover.com>
2015-10-12 11:05:38 -0700

    add check for undefined led pin

--- canbus-firewall-avr/src/led.h
+++ canbus-firewall-avr/src/led.h
@@ -12,6 +12,19 @@
 #define LED_LOW 0
 #define LED_HIGH 1
 
+#ifndef LED_01_PIN
+#error LED_01_PIN NOT SET
+#else
+#define LED_01	LED_01_PIN
+#endif
+
+#ifndef LED_02_PIN
+#error LED_02_PIN NOT SET
+#else
+#define LED_02	LED_02_PIN
+#endif
+
+
 #define LED_ON	LED_LOW		//low = on
 #define LED_OFF	LED_HIGH	//high = off
 
