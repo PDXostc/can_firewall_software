1b4cc594e896ed99be0f6dbd9f532df157c569ff
Steve Miller <smiller6@jaguarlandrover.com>
2015-10-14 10:37:24 -0700

    add mcp stub files

---
+++ canbus-firewall-avr/src/mcp.c
@@ -0,0 +1,8 @@
+/*
+ * mcp.c
+ *
+ * Created: 10/14/2015 10:28:10 AM
+ *  Author: smiller6
+ */ 
+
+#include "mcp.h"
---
+++ canbus-firewall-avr/src/mcp.h
@@ -0,0 +1,17 @@
+/*
+ * mcp.h
+ *
+ * Created: 10/14/2015 10:27:41 AM
+ *  Author: smiller6
+ */ 
+
+#include <asf.h>
+
+#ifndef MCP_H_
+#define MCP_H_
+
+
+
+
+
+#endif /* MCP_H_ */
